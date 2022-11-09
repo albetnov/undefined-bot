10bd5b4a14ff026c7cb8e8d786bb876a2510e81f
albetnov <ryxinfrvr@gmail.com>
2022-11-09 14:40:45 +0700

    refactor clear message

--- src/Commands/ClearMessage.ts
+++ src/Commands/ClearMessage.ts
@@ -1,4 +1,5 @@
-import { ChannelType, ChatInputCommandInteraction } from "discord.js";
+import { ChannelType, ChatInputCommandInteraction, Message } from "discord.js";
+import { setTimeout } from "timers/promises";
 import BaseCommand from "../Utils/BaseCommand";
 
 export default class ClearMessage extends BaseCommand {
@@ -11,12 +12,59 @@ export default class ClearMessage extends BaseCommand {
   }
 
   async handler(action: ChatInputCommandInteraction) {
-    if (action.channel?.type !== ChannelType.GuildText) return;
-    let fetched;
-    do {
-      fetched = await action.channel?.messages.fetch({ limit: 100 });
-      action.channel.bulkDelete(fetched);
-    } while (fetched.size >= 2);
-    action.reply("Channel cleared successfully.");
+    if (!action.memberPermissions?.has("Administrator")) {
+      action.reply({ content: "Only admin can do this!", ephemeral: true });
+    }
+
+    const user = action.user.id;
+
+    action.reply({
+      content: "Are you sure you want to clear this channel message?",
+      ephemeral: true,
+    });
+
+    const filter = (collect: Message) => {
+      if (collect.member === null || collect.member.id !== user) return false;
+      return collect.content.toLowerCase() === "yes" || collect.content.toLowerCase() === "no";
+    };
+
+    const collector = action.channel?.createMessageCollector({
+      filter,
+      max: 1,
+      time: 30000,
+    });
+
+    collector?.on("collect", async (collected) => {
+      const clear = collected.content === "yes" ? true : false;
+      if (clear) {
+        collected.reply({ content: "Clearing Channel", options: { ephemeral: true } });
+
+        await setTimeout(1000);
+
+        if (action.channel?.type !== ChannelType.GuildText) {
+          collected.reply({
+            content: "Incompitable channel type: Channel Must Text Based.",
+            options: { ephemeral: true },
+          });
+          return;
+        }
+
+        let fetched;
+
+        do {
+          fetched = await action.channel?.messages.fetch({ limit: 100 });
+          action.channel.bulkDelete(fetched);
+        } while (fetched.size >= 2);
+      }
+
+      action.followUp({ content: "You said no: Aborting.", ephemeral: true });
+    });
+
+    collector?.on("end", (collected) => {
+      if (collected.size === 0) {
+        action.followUp({ content: "No Input: Aborting.", ephemeral: true });
+        return;
+      }
+    });
   }
 }
