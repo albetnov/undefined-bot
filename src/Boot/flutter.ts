576dd2c6cce5abc9120c0ab692402cb3694b3ebf
albetnov <ryxinfrvr@gmail.com>
2022-11-16 16:20:00 +0700

    fix deploy not working

--- src/Boot/flutter.ts
+++ src/Boot/flutter.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Client, EmbedBuilder, hyperlink } from "discord.js";
+import { ChannelType, Client, EmbedBuilder } from "discord.js";
 import { gracefulShutdown, scheduleJob } from "node-schedule";
 import { logger } from "..";
 import { getCacheByKey } from "../Utils/GetCache";
@@ -19,63 +19,12 @@ export default async function flutter(client: Client) {
 
   const PHOTO = "https://i.pinimg.com/564x/ee/fe/5f/eefe5f009c7105b18b8df4d86f907ce3.jpg";
 
-  scheduleJob(new Date(2022, 10, 6, 21, 48, 0), () => {
+  scheduleJob({ hour: 12 }, () => {
     const embeds = new EmbedBuilder()
-      .setTitle("Flutter Roadmap, Begin!")
-      .setDescription(
-        `
-      Your first assignment would be so simple, all you need to do is simply preparing your environment
-      for developing Flutter and of course Dart.
-
-      Here is, your starting point:
-      ${hyperlink("Installing Dart by Mr Erico", "https://youtu.be/asNdz10WR6w")}
-
-      ------------------------------------------------------------
-
-      That's all from me, meet again at your week's 4. Shall we?
-      `
-      )
+      .setTitle("Testing Cron Job")
+      .setDescription(`I am still testing the cron job`)
       .setImage(PHOTO);
 
-    channel.send({ embeds: [embeds] });
-  });
-
-  scheduleJob(new Date(2022, 10, 7, 23, 10, 0), () => {
-    const embeds = new EmbedBuilder()
-      .setTitle("Your Updates: First.")
-      .setDescription(
-        `
-      @everyone,
-      I will not remind daily. The author do plan to make a daily reminder, for weeks percentage.
-      But the author still developing me. So until then, you can wait.
-      
-      Back to topic. I hope that you can targeting until this:
-      ${hyperlink("List & Mapping", "https://youtu.be/Jy4elW9Y64M")}
-
-      ------------------------------------------------------------
-
-      That's all from me, you may enjoy your activities.
-      `
-      )
-      .setImage(PHOTO);
-
-    channel.send({ embeds: [embeds] });
-  });
-
-  scheduleJob(new Date(2022, 10, 8, 20, 0, 0), () => {
-    const embeds = new EmbedBuilder()
-      .setTitle("Your Updates: Second.")
-      .setDescription(
-        `
-      @everyone,
-
-      I hope you all at least achieving 
-      ${hyperlink("Upgrade Your Monster with Mixins.", "https://youtu.be/jKb3_EuJaRE")}
-      
-      That's all.
-      `
-      )
-      .setImage(PHOTO);
     channel.send({ embeds: [embeds] });
   });
 }
--- src/Utils/GetCache.ts
+++ src/Utils/GetCache.ts
@@ -5,16 +5,16 @@ interface KeyValueSchema {
   value: string;
 }
 
-export function getCache(fileName: string): object[] {
+export function getCache(fileName: string): KeyValueSchema[] {
   const path = `${__dirname}/../Cache/${fileName}.json`;
-  let result = [{}];
+  let result = [{ name: "", value: "" }];
   if (fs.existsSync(path)) {
-    result = JSON.parse(fs.readFileSync(path, { encoding: "utf-8" })) as object[];
+    result = JSON.parse(fs.readFileSync(path, { encoding: "utf-8" }));
   }
   return result;
 }
 
 export function getCacheByKey(fileName: string, key: string): string {
   const result = getCache(fileName);
-  return (result.filter((item: any) => item.name === key)[0] as KeyValueSchema).value;
+  return result.filter((item: KeyValueSchema) => item.name === key)[0].value;
 }
