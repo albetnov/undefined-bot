55ab94c885769268d12151880cdd78dc520a9e86
Albet Nov <ryxinfrvr@gmail.com>
2022-11-16 23:12:53 +0700

    create cache folder if not exist

--- src/Boot/refreshCache.ts
+++ src/Boot/refreshCache.ts
@@ -1,9 +1,16 @@
 import { Client } from "discord.js";
+import { existsSync, mkdirSync } from "fs";
 import { logger } from "..";
 import refreshChannels from "../Refresher/RefreshChannels";
 import refreshRoles from "../Refresher/RefreshRoles";
 
 export default function refreshCache(client: Client) {
+  const dir = __dirname + "/../Cache/";
+
+  if (!existsSync(dir)) {
+    mkdirSync(dir);
+  }
+
   refreshChannels(client);
   refreshRoles();
   logger.info("Cache refreshed!");
