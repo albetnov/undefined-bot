4d37b77ea9ec5f2f7c7e63cdf54c98ccd271c810
albetnov <ryxinfrvr@gmail.com>
2022-11-22 16:11:35 +0700

    fix status code

--- src/Webhook/WebhookHttp.ts
+++ src/Webhook/WebhookHttp.ts
@@ -20,20 +20,20 @@ export default class WebhookHttp {
           const token = await new TokenRepository().find("token");
 
           if (!token.exists()) {
-            reply.send({ error: "Token not found. Please try again" });
+            reply.status(403).send({ error: "Token not found. Please try again" });
             return;
           }
 
           const data = token.data();
 
           if (data!.expire_at.toDate() < new Date()) {
-            reply.send({ error: "Token expired. Please try again." });
+            reply.status(403).send({ error: "Token expired. Please try again." });
             return;
           }
 
           const query = request.query as any;
           if (query.adminKey !== data!.value) {
-            reply.send({ error: "Token mismatch" });
+            reply.status(403).send({ error: "Token mismatch" });
             return;
           }
         });
