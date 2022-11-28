ff31a7e8666b308124912ccb401c19033821c214
albetnov <ryxinfrvr@gmail.com>
2022-11-28 11:32:56 +0700

    add validation for refresh cache

--- src/WSClient/Listeners/RefreshCache.ts
+++ src/WSClient/Listeners/RefreshCache.ts
@@ -1,20 +1,34 @@
 import refreshChannels from "../../Refresher/RefreshChannels";
 import refreshRoles from "../../Refresher/RefreshRoles";
 import BaseListener, { SocketListenerHandler } from "./BaseListener";
+import { z } from "zod";
 
 export default class RefreshCache extends BaseListener {
   socket: string = "RefreshCache";
 
   handler({ params }: SocketListenerHandler) {
-    const body = params[0];
+    const [body, callback] = params;
 
-    if ("channels" in body && body.channels) {
-      refreshChannels();
-    } else if ("roles" in body && body.roles) {
-      refreshRoles();
-    } else {
-      refreshChannels();
-      refreshRoles();
+    const bodySchema = z.object({
+      channels: z.boolean().optional(),
+      roles: z.boolean().optional(),
+    });
+
+    const result = bodySchema.safeParse(body);
+
+    if (result.success) {
+      if (result.data.channels) {
+        refreshChannels();
+      } else if (result.data.roles) {
+        refreshRoles();
+      } else {
+        refreshChannels();
+        refreshRoles();
+      }
     }
+
+    callback({
+      success: result.success,
+    });
   }
 }
