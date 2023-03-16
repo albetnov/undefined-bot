e2c51b9c003aef65c7cb3005f62619de4e9d065a
albetnov <ryxinfrvr@gmail.com>
2023-03-16 15:57:31 +0700

    chore: update random array to use crypto algo

--- src/Utils/random.ts
+++ src/Utils/random.ts
@@ -1,3 +1,7 @@
+import crypto from "crypto";
+
 export default function random<T>(array: T[]): T {
-  return array[Math.floor(Math.random() * array.length)];
+  const randomBuffer = crypto.randomBytes(4);
+  const randomIndex = randomBuffer.readUInt32BE(0) % array.length;
+  return array[randomIndex];
 }
